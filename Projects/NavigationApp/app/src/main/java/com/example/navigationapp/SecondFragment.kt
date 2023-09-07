2aef2709cbe3afac8b73551b9302e4bb8db2a917
Abdul Sattar <abdulsattar.cheemag@gmail.com>
2023-09-07 08:43:14 -0700

    using navigation fragment

---
+++ Projects/NavigationApp/app/src/main/java/com/example/navigationapp/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.navigationapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navigationapp.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container,false)
+        //receiving data from home frag
+        val input = arguments!!.getString("name")
+        binding.name.text = input.toString()
+        return binding.root
+    }
+
+}
